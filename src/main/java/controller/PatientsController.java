268ef30f55052b25160cb2bfcb3c53e905fabcad
Thagus <agu.alcarin@gmail.com>
2018-06-06 21:24:47 -0300

    Added base patients view and controlelr

--- src/main/java/controller/PatientsController.java
+++ src/main/java/controller/PatientsController.java
@@ -2,9 +2,15 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import org.controlsfx.control.textfield.CustomTextField;
 
 public class PatientsController {
 
-    public void handleBar() {
+    @FXML private CustomTextField searchField;
+    @FXML private TableView patientsTable;
+
+    public void init() {
+
     }
 }
