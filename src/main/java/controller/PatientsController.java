28f256d84f3e1b492aa153b5b69403868d56a142
Thagus <agu.alcarin@gmail.com>
2018-06-06 22:02:46 -0300

    Added missing in patients

--- src/main/java/controller/PatientsController.java
+++ src/main/java/controller/PatientsController.java
@@ -1,7 +1,9 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import org.controlsfx.control.textfield.CustomTextField;
 
@@ -10,7 +12,17 @@ public class PatientsController {
     @FXML private CustomTextField searchField;
     @FXML private TableView patientsTable;
 
+    @FXML private TableColumn nameColumn;
+    @FXML private TableColumn lastnameColumn;
+    @FXML private TableColumn gender;
+    @FXML private TableColumn recordColumn;
+    @FXML private TableColumn consultationColumn;
+    @FXML private TableColumn deleteColumn;
+
     public void init() {
 
+    }
+
+    public void newPatient(ActionEvent actionEvent) {
     }
 }
