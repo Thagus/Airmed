564aabd7f42057c255ea1af65c1ba542a8a792c4
Thagus <agu.alcarin@gmail.com>
2018-06-06 22:03:11 -0300

    Added treatments view and controller

--- src/main/java/controller/TreatmentsController.java
+++ src/main/java/controller/TreatmentsController.java
@@ -1,4 +1,24 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import org.controlsfx.control.textfield.CustomTextField;
+
 public class TreatmentsController {
+
+    @FXML private TableView treatmentsTable;
+    @FXML private CustomTextField searchField;
+
+    @FXML private TableColumn descriptionColumn;
+    @FXML private TableColumn nameColumn;
+    @FXML private TableColumn editColumn;
+    @FXML private TableColumn deleteColumn;
+
+    public void init() {
+    }
+
+    public void newTreatment(ActionEvent actionEvent) {
+    }
 }
