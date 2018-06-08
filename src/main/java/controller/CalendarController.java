472ce4421c3d8f3466243457fcb805dad7c5c351
Thagus <agu.alcarin@gmail.com>
2018-06-07 22:08:13 -0300

    Changed Calendar to use jfxtras instead of CalendarFX due to internationalization issues

--- src/main/java/controller/CalendarController.java
+++ src/main/java/controller/CalendarController.java
@@ -1,45 +1,22 @@
 package controller;
 
-import com.calendarfx.view.DetailedWeekView;
-import com.calendarfx.view.MonthView;
-import com.calendarfx.view.YearView;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.ScrollPane;
 import jfxtras.scene.control.CalendarPicker;
+import jfxtras.scene.control.agenda.Agenda;
 import org.controlsfx.control.textfield.CustomTextField;
 
+import java.util.Locale;
+
 public class CalendarController {
+
+
+    @FXML private Agenda agendaView;
 
     @FXML private CustomTextField searchField;
 
     @FXML private CalendarPicker calendarPicker;
 
-    @FXML private DetailedWeekView weekView;
-    @FXML private MonthView monthView;
-    @FXML private ScrollPane yearScrollPane;
-    @FXML private YearView yearView;
-
-
-
     public void init(){
-    }
-
-    public void viewWeek(ActionEvent actionEvent) {
-        monthView.setVisible(false);
-        yearScrollPane.setVisible(false);
-        weekView.setVisible(true);
-    }
-
-    public void viewMonth(ActionEvent actionEvent) {
-        weekView.setVisible(false);
-        yearScrollPane.setVisible(false);
-        monthView.setVisible(true);
-    }
-
-    public void viewYear(ActionEvent actionEvent) {
-        weekView.setVisible(false);
-        monthView.setVisible(false);
-        yearScrollPane.setVisible(true);
+        agendaView.setLocale(new Locale("es", "mx"));
     }
 }
