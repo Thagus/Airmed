2ad4c154722033c04b946cd4a1fd957bba263b5e
Thagus <agu.alcarin@gmail.com>
2018-06-07 22:08:37 -0300

    Added studies view and controller

--- src/main/java/controller/StudiesController.java
+++ src/main/java/controller/StudiesController.java
@@ -1,4 +1,25 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import org.controlsfx.control.textfield.CustomTextField;
+
 public class StudiesController {
+
+    @FXML private CustomTextField searchField;
+
+    @FXML private TableView studiesTable;
+
+    @FXML private TableColumn nameColumn;
+    @FXML private TableColumn descriptionColumn;
+    @FXML private TableColumn editColumn;
+    @FXML private TableColumn deleteColumn;
+
+    public void init() {
+    }
+
+    public void newStudy(ActionEvent actionEvent) {
+    }
 }
