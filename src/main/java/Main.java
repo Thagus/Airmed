1b1fe0f922f81f9769fea48fb155d8eba1b46525
Thagus <agu.alcarin@gmail.com>
2018-09-11 23:48:42 -0500

    Switched to programmatically connecting to db to allow more flexibility and db encryption

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
 import controller.MenuController;
+import io.ebean.EbeanServer;
+import io.ebean.EbeanServerFactory;
+import io.ebean.config.DbMigrationConfig;
+import io.ebean.config.ServerConfig;
+import io.ebean.config.dbplatform.h2.H2DbEncrypt;
+import io.ebeaninternal.dbmigration.DdlGenerator;
+import io.ebeaninternal.server.core.DefaultServer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import model.Setting;
+import org.avaje.datasource.DataSourceConfig;
 
 import java.io.IOException;
 import java.util.Locale;
@@ -14,6 +23,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+        createDatabase();
+
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("Airmed");
 
@@ -31,6 +42,26 @@ public class Main extends Application {
 
         this.primaryStage.setScene(scene);
         this.primaryStage.show();
+    }
+
+    private void createDatabase(){
+        DataSourceConfig dsConfig = new DataSourceConfig();
+        dsConfig.setUsername("airmed");
+        dsConfig.setPassword("airmed airmed");
+        dsConfig.setUrl("jdbc:h2:" + System.getProperty("user.home") + "/.db/airmed;TRACE_LEVEL_FILE=0;CIPHER=AES");
+        dsConfig.setDriver("org.h2.Driver");
+
+        ServerConfig config = new ServerConfig();
+        config.setName("airmed");
+        config.setDataSourceConfig(dsConfig);
+        config.setDefaultServer(true);
+
+        //config.setDdlGenerate(true);
+        //config.setDdlRun(true);
+
+        EbeanServer server = EbeanServerFactory.create(config);
+
+        //Run migrations
     }
 
     public Stage getPrimaryStage() {
