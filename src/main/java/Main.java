e9f8388e4d2a5d7b53c66574054c8666b9192181
Thagus <agu.alcarin@gmail.com>
2018-09-18 22:55:37 -0500

    Made a proper functioning login with a shake transition

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,22 @@ import io.ebean.EbeanServer;
 import io.ebean.EbeanServerFactory;
 import io.ebean.config.ServerConfig;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventType;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.TextInputDialog;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import org.avaje.datasource.DataSourceConfig;
+import utils.ShakeTransition;
+
 import java.io.IOException;
 import java.util.Locale;
-import java.util.Optional;
 
 public class Main extends Application {
 
@@ -19,12 +26,95 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        createDatabase();
-
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("Airmed");
-
         Locale.setDefault(new Locale("es", "mx"));
+        this.primaryStage = primaryStage;
+
+        databaseLogin();
+    }
+
+    private void databaseLogin(){
+        DataSourceConfig dsConfig = new DataSourceConfig();
+        dsConfig.setUsername("airmed");
+        dsConfig.setUrl("jdbc:h2:" + System.getProperty("user.home") + "/.db/airmed;TRACE_LEVEL_FILE=0;CIPHER=AES");
+        dsConfig.setDriver("org.h2.Driver");
+
+        ServerConfig config = new ServerConfig();
+        config.setName("airmed");
+        config.setDefaultServer(true);
+
+        //Create prompt dialog
+        Dialog dialog = new Dialog();
+        dialog.setTitle("Airmed");
+        dialog.setHeaderText("Acceso al sistema");
+        dialog.initModality(Modality.NONE);
+
+        //Create buttons
+        ButtonType loginButtonType = new ButtonType("Login", ButtonBar.ButtonData.OK_DONE);
+        dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);
+
+        //Create pane
+        HBox box = new HBox();
+        box.setSpacing(10);
+
+        //Add password label and field to the layout
+        PasswordField passwordField = new PasswordField();
+        box.getChildren().addAll(new Label("Password :"), passwordField);
+
+        //Disable login button by default
+        Node loginButton = dialog.getDialogPane().lookupButton(loginButtonType);
+        loginButton.setDisable(true);
+        //Enable button if there's text in the password field
+        passwordField.textProperty().addListener((observable, oldValue, newValue) -> loginButton.setDisable(newValue.trim().isEmpty()));
+
+        dialog.getDialogPane().setContent(box);
+
+        // Request focus on the player name field by default.
+        Platform.runLater(passwordField::requestFocus);
+
+        dialog.initModality(Modality.APPLICATION_MODAL);
+        dialog.show();
+
+        //Add listener for when the button is pressed
+        loginButton.addEventFilter(EventType.ROOT,
+                e->{
+                    if(e.getEventType().equals(ActionEvent.ACTION)){
+                        e.consume();
+
+                        //Get password and verify is not empty
+                        String password = passwordField.getText();
+                        if (password.length()>0) {
+                            //Set the DB password
+                            dsConfig.setPassword(password + " " + password);
+
+                            //Initialize server config
+                            config.setDataSourceConfig(dsConfig);
+
+                            //Try to initiate db connection
+                            try {
+                                EbeanServer server = EbeanServerFactory.create(config);
+
+                                /*
+                                //TODO: Run migrations
+                                MigrationConfig migrationConfig = new MigrationConfig();
+                                migrationConfig.set
+                                MigrationRunner runner = new MigrationRunner();
+                                */
+
+                                //Close the dialog and initiate primary stage
+                                dialog.close();
+                                createPrimaryStage();
+                            }
+                            catch (Exception ignored){  //If the login fails, shake the dialog and ask for the password again
+                                ShakeTransition anim = new ShakeTransition(dialog.getDialogPane(), t->passwordField.requestFocus());
+                                anim.playFromStart();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void createPrimaryStage() throws IOException {
+        this.primaryStage.setTitle("Airmed");
 
         Scene scene = new Scene(new StackPane());
 
@@ -38,55 +128,6 @@ public class Main extends Application {
 
         this.primaryStage.setScene(scene);
         this.primaryStage.show();
-    }
-
-    private void createDatabase() {
-        DataSourceConfig dsConfig = new DataSourceConfig();
-        dsConfig.setUsername("airmed");
-        dsConfig.setUrl("jdbc:h2:" + System.getProperty("user.home") + "/.db/airmed;TRACE_LEVEL_FILE=0;CIPHER=AES");
-        dsConfig.setDriver("org.h2.Driver");
-
-        boolean aprooved = false;
-
-        //Get password from user
-        TextInputDialog dialog = new TextInputDialog();
-        dialog.setTitle("Contraseña de BD");
-        dialog.setHeaderText(null);
-        dialog.setContentText("Contraseña:");
-        dialog.setOnCloseRequest(event ->
-            System.exit(0)
-        );
-
-        do {
-            Optional<String> result = dialog.showAndWait();
-
-            if(result.isPresent()){
-                String password = result.get();
-                if(password.length()>0) {
-                    dsConfig.setPassword(password + " " + password);
-
-                    ServerConfig config = new ServerConfig();
-                    config.setName("airmed");
-                    config.setDataSourceConfig(dsConfig);
-                    config.setDefaultServer(true);
-
-                    try {
-                        EbeanServer server = EbeanServerFactory.create(config);
-
-                        aprooved = true;
-
-                        /*
-                        //TODO: Run migrations
-                        MigrationConfig migrationConfig = new MigrationConfig();
-                        migrationConfig.set
-                        MigrationRunner runner = new MigrationRunner();
-                        */
-                    }
-                    catch (Exception ignored){}
-                }
-            }
-        }
-        while (!aprooved);
     }
 
     public Stage getPrimaryStage() {
