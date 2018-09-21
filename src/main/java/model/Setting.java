a4c1a5f721303f8bd646bd39088df15c308ba226
Thagus <agu.alcarin@gmail.com>
2018-09-21 16:41:43 -0500

    Added setting finder and creator

--- src/main/java/model/Setting.java
+++ src/main/java/model/Setting.java
@@ -1,5 +1,6 @@
 package model;
 
+import io.ebean.Finder;
 import io.ebean.Model;
 
 import javax.persistence.Column;
@@ -13,4 +14,28 @@ public class Setting extends Model {
 
     @Column(nullable = false)
     private String value;
+
+    public static Finder<String, Setting> find = new Finder<>(Setting.class);
+
+    public static Setting create (String setting, String value) {
+        Setting settingObj = new Setting();
+
+        settingObj.setting = setting;
+        settingObj.value = value;
+
+        settingObj.save();
+        return settingObj;
+    }
+
+    public String getSetting() {
+        return setting;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 }
