b38135a1dfdf88d8e38efb35f6e31f5a6a2b6bfb
Thagus <agu.alcarin@gmail.com>
2018-09-27 23:49:56 -0500

    Added creator, finder, getters and setters to Study

--- src/main/java/model/Study.java
+++ src/main/java/model/Study.java
@@ -1,5 +1,6 @@
 package model;
 
+import io.ebean.Finder;
 import io.ebean.Model;
 
 import javax.persistence.Column;
@@ -20,4 +21,40 @@ public class Study extends Model {
 
     @ManyToMany(mappedBy = "studies")
     private List<Prescription> prescriptions;
+
+    public static Finder<Integer, Study> find = new Finder<>(Study.class);
+
+    public static Study create(String name, String description) {
+        Study study = new Study();
+
+        study.name = name;
+        study.description = description;
+
+        study.save();
+        return study;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Prescription> getPrescriptions() {
+        return prescriptions;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
