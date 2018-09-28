09858711cbdd5b436e18bf0d23f358bdb65d5020
Thagus <agu.alcarin@gmail.com>
2018-09-27 23:55:11 -0500

    Added contructor, finder, getters and setters to Treatment

--- src/main/java/model/Treatment.java
+++ src/main/java/model/Treatment.java
@@ -1,5 +1,6 @@
 package model;
 
+import io.ebean.Finder;
 import io.ebean.Model;
 
 import javax.persistence.Column;
@@ -23,4 +24,49 @@ public class Treatment extends Model {
 
     @ManyToMany(mappedBy = "treatments")
     private List<Prescription> prescriptions;
+
+    public static Finder<Integer, Treatment> find = new Finder<>(Treatment.class);
+
+    public static Treatment create(String name, String description, List<Dose> medicines){
+        Treatment treatment = new Treatment();
+
+        treatment.name = name;
+        treatment.description = description;
+        treatment.medicines = medicines;
+
+        treatment.save();
+        return treatment;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Dose> getMedicines() {
+        return medicines;
+    }
+
+    public List<Prescription> getPrescriptions() {
+        return prescriptions;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setMedicines(List<Dose> medicines) {
+        this.medicines = medicines;
+    }
 }
