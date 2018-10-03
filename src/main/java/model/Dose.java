cec5f4ddeb92e669f96eca6873f04fdab835e9b5
Thagus <agu.alcarin@gmail.com>
2018-10-03 01:09:32 -0500

    Added creator, finder, getters and setters to Dose and Medicine

--- src/main/java/model/Dose.java
+++ src/main/java/model/Dose.java
@@ -1,5 +1,6 @@
 package model;
 
+import io.ebean.Finder;
 import io.ebean.Model;
 
 import javax.persistence.*;
@@ -18,4 +19,40 @@ public class Dose extends Model {
 
     @ManyToMany(mappedBy = "medicines")
     private List<Treatment> treatments;
+
+    public static Finder<Integer, Dose> find = new Finder<>(Dose.class);
+
+    public static Dose create(String dose, Medicine medicine) {
+        Dose doseObj = new Dose();
+
+        doseObj.dose = dose;
+        doseObj.medicine = medicine;
+
+        doseObj.save();
+        return doseObj;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDose() {
+        return dose;
+    }
+
+    public Medicine getMedicine() {
+        return medicine;
+    }
+
+    public List<Treatment> getTreatments() {
+        return treatments;
+    }
+
+    public void setDose(String dose) {
+        this.dose = dose;
+    }
+
+    public void setMedicine(Medicine medicine) {
+        this.medicine = medicine;
+    }
 }
--- src/main/java/model/Medicine.java
+++ src/main/java/model/Medicine.java
@@ -1,5 +1,6 @@
 package model;
 
+import io.ebean.Finder;
 import io.ebean.Model;
 
 import javax.persistence.*;
@@ -17,4 +18,40 @@ public class Medicine extends Model {
 
     @OneToMany(mappedBy = "medicine")
     private List<Dose> doses;
+
+    public static Finder<Integer, Medicine> find = new Finder<>(Medicine.class);
+
+    public static Medicine create(String name, String description) {
+        Medicine medicine = new Medicine();
+
+        medicine.name = name;
+        medicine.description = description;
+
+        medicine.save();
+        return medicine;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Dose> getDoses() {
+        return doses;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
