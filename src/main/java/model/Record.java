0a3412374ac2ff86118f07d8c19d167fa62a7245
Thagus <agu.alcarin@gmail.com>
2018-10-09 00:07:04 -0500

    Added creator, getters and setters to Record

--- src/main/java/model/Record.java
+++ src/main/java/model/Record.java
@@ -12,7 +12,7 @@ public class Record extends Model {
     private int id;
 
     @OneToOne
-    @JoinColumn(name = "id")
+    @JoinColumn(name = "id", nullable = false)
     private Patient patient;
 
     @Column(columnDefinition = "clob")
@@ -37,4 +37,69 @@ public class Record extends Model {
     private List<Surgery> surgeries;
     
     public static Finder<Integer, Record> find = new Finder<>(Record.class);
+
+    public static Record create(Patient patient){
+        Record record = new Record();
+
+        record.patient = patient;
+
+        record.save();
+        return record;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public String getFamilyBg() {
+        return familyBg;
+    }
+
+    public String getPersonalBg() {
+        return personalBg;
+    }
+
+    public String getSystemsBg() {
+        return systemsBg;
+    }
+
+    public String getAllergies() {
+        return allergies;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public List<StudyResult> getStudyResults() {
+        return studyResults;
+    }
+
+    public List<Surgery> getSurgeries() {
+        return surgeries;
+    }
+
+    public void setFamilyBg(String familyBg) {
+        this.familyBg = familyBg;
+    }
+
+    public void setPersonalBg(String personalBg) {
+        this.personalBg = personalBg;
+    }
+
+    public void setSystemsBg(String systemsBg) {
+        this.systemsBg = systemsBg;
+    }
+
+    public void setAllergies(String allergies) {
+        this.allergies = allergies;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
 }
