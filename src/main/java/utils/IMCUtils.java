ff6a88dca8ebdbe510d481c1717e36a5cb17c593
Thagus <agu.alcarin@gmail.com>
2019-01-29 13:26:41 -0600

    Added base method for IMC interpretation

--- src/main/java/utils/IMCUtils.java
+++ src/main/java/utils/IMCUtils.java
@@ -2,9 +2,14 @@ package utils;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
 
 public class IMCUtils {
     public static BigDecimal calculateIMC(int weight, int height){
         return BigDecimal.valueOf(((double)weight/((double)height*(double)height))*10).setScale(2, RoundingMode.CEILING);
+    }
+
+    public static String interpretIMC(BigDecimal imc, LocalDate birthdate){
+        return "";
     }
 }
