439b587668472f91453b34de839bbe5d8f9b24ba
Thagus <agu.alcarin@gmail.com>
2019-02-19 17:02:36 -0600

    Fixed naming issue in dbmigration

--- src/main/resources/dbmigration/1.4.sql
+++ src/main/resources/dbmigration/1.4.sql
@@ -6,17 +6,17 @@ create table disease (
   constraint pk_disease primary key (id)
 );
 
-create table disease_controlled (
+create table disease_status (
   id                            integer auto_increment not null,
   controlled                    boolean default false not null,
   disease_id                    integer,
   consultation_id               integer,
-  constraint pk_disease_controlled primary key (id)
+  constraint pk_disease_status primary key (id)
 );
 
-create index ix_disease_controlled_disease_id on disease_controlled (disease_id);
-alter table disease_controlled add constraint fk_disease_controlled_disease_id foreign key (disease_id) references disease (id) on delete restrict on update restrict;
+create index ix_disease_status_disease_id on disease_status (disease_id);
+alter table disease_status add constraint fk_disease_status_disease_id foreign key (disease_id) references disease (id) on delete restrict on update restrict;
 
-create index ix_disease_controlled_consultation_id on disease_controlled (consultation_id);
-alter table disease_controlled add constraint fk_disease_controlled_consultation_id foreign key (consultation_id) references consultation (id) on delete restrict on update restrict;
+create index ix_disease_status_consultation_id on disease_status (consultation_id);
+alter table disease_status add constraint fk_disease_status_consultation_id foreign key (consultation_id) references consultation (id) on delete restrict on update restrict;
 
